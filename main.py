edc6bb1d5ab165d71fdc2741fc49939ccca70945
dungatoro <noahjbarker@icloud.com>
2024-04-14 14:18:19 +0100

    imported cmd

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# test on https://www.puzzle-words.com/boggle-5x5-extreme/ 
+
+import cmd
+
 def create_graph(x, y):
     # creates the adjacency list dictionary for a boggle graph
     graph = {}
@@ -25,7 +29,7 @@ def word_is_possible(word, letters):
             return False
     return True
 
-def path_exists(word, graph, letters, start):
+def find_path(word, graph, letters):
     # check if the word can be made by travelling from a given starting node
     if len(word) == 1:
         if word in letters:
@@ -34,60 +38,73 @@ def path_exists(word, graph, letters, start):
     if not word:
         return false
     
-    stack = [start]
-    needed = list(word[1::][::-1]) # top is the next letter to find
-    visited = [start]
-
-    current = start
-    letter_needed = needed.pop()
-    while stack:
-        # find the valid exits onto the next letter in the word (you cannot 
-        # backtrack onto a node you have already visited)
-        exits = [i for i in graph[current] if letters[i] == letter_needed and i not in visited]
-        if not exits:
-            needed.append(letter_needed)
-            letter_needed = letters[current]
-            current = stack.pop()
-        else:
-            if not needed: 
-                # reached the final letter :. a path exists!
-                return True
-
-            stack.append(current) # add current to the stack to backtrack to if needed
-            current = exits[0] # move on to the next exit
-            visited.append(current) # mark the new node as visited
-            letter_needed = needed.pop() # get the next letter needed to make the word
-    return False
-
-def word_in_graph(word, graph, letters):
     # find all the potential start points
     starts = [i for i, c in enumerate(letters) if c == word[0]]
     for start in starts:
-        # check each start point until a path is found
-        if path_exists(word, graph, letters, start):
-            return True
-    # if a path is not found we return False as the word is not valid
-    return False
-        
-# test on https://www.puzzle-words.com/boggle-5x5-extreme/ 
+        stack = [start]
+        needed = list(word[1::][::-1]) # top is the next letter to find
+        visited = [start]
 
-# change depending on the boggle 
-letters = "nasnsaztsertrihsgimroniwn" # each row, left to right
-graph = create_graph(5, 5) # (x, y) dimensions of the boggle grid
+        current = start
+        letter_needed = needed.pop()
+        while stack:
+            # find the valid exits onto the next letter in the word (you cannot 
+            # backtrack onto a node you have already visited)
+            exits = [i for i in graph[current] if letters[i] == letter_needed and i not in visited]
+            if not exits:
+                needed.append(letter_needed)
+                letter_needed = letters[current]
+                current = stack.pop()
+            else:
+                if not needed: 
+                    # reached the final letter :. a path exists!
+                    return stack
+
+                stack.append(current) # add current to the stack to backtrack to if needed
+                current = exits[0] # move on to the next exit
+                visited.append(current) # mark the new node as visited
+                letter_needed = needed.pop() # get the next letter needed to make the word
+    return []
+
+def get_answers(graph, letters):
+    answers = []
+    for word in open('words.txt','r').readlines():
+        word = word.strip() # remove whitespace
+        path = find_path(word, graph, letters)
+        if word_is_possible(word, letters) and path:
+            # the word is a valid answer
+            answers.append(word)
+
+    return sorted(answers, key=len, reverse=True) # answers sorted from longest
+
+class App(cmd.Cmd):
+    letters = "ousectesoreuabhtlfogcaesy"
+    grid = {}
+    prompt = f'{letters} >> '
+
+    def do_set_letters(self, letters):
+        """
+        Set the letters in the grid (read left to right, row by row)
+        """
+        self.letters = letters
+
+    def do_set_grid(self, dimensions):
+        """
+        Set the dimensions of the grid
+        """
+        x, y = map(int, dimensions.split())
+        self.grid = create_graph(x, y)
     
-answers = []
-for word in open('words.txt','r').readlines():
-    word = word.strip() # remove whitespace
-    if word_is_possible(word, letters) and word_in_graph(word, graph, letters):
-        # the word is a valid answer
-        answers.append(word)
-
-# print out the answers from biggest to smallest (at cli use `| less` to scroll through)
-answers = sorted(answers, key=len, reverse=True)
-for answer in answers:
-    print(answer)
+    def do_top_words(self, num):
+        """
+        Print the top `num` words
+        """
+        num = num or 5
+        answers = get_answers(self.grid, self.letters)
+        for i in range(int(num)):
+            print(answers[i])
 
 
-
-        
+if __name__ == "__main__":
+    App().cmdloop()    
 
